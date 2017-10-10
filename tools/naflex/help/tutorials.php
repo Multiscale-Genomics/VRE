ad85c9942383a3c99888da5de2dc72cade3255f7
Laia <laia.codo@bsc.es>
2017-10-10 17:06:09 +0200

    updates pydockdna and naflex help. fix tool skeleton

---
+++ tools/naflex/help/tutorials.php
@@ -0,0 +1,66 @@
+<?php
+
+require "../../../phplib/genlibraries.php";
+redirectOutside();
+
+?>
+
+<?php require "../../../htmlib/header.inc.php"; ?>
+
+<body class="page-header-fixed page-sidebar-closed-hide-logo page-content-white page-container-bg-solid page-sidebar-fixed">
+  <div class="page-wrapper">
+
+  <?php require "../../../htmlib/top.inc.php"; ?>
+  <?php require "../../../htmlib/menu.inc.php"; ?>
+
+	<!-- BEGIN CONTENT -->
+		<div class="page-content-wrapper">
+				<!-- BEGIN CONTENT BODY -->
+				<div class="page-content" id="body-help">
+						<!-- BEGIN PAGE HEADER-->
+						<!-- BEGIN PAGE BAR -->
+						<div class="page-bar">
+								<ul class="page-breadcrumb">
+									<li>
+										<a href="/home/">Home</a>
+										<i class="fa fa-circle"></i>
+									</li>
+									<li>
+											<span>Help</span>
+											<i class="fa fa-circle"></i>
+									</li>
+									<li>
+											<a href="help/tools.php">Tools</a>
+											<i class="fa fa-circle"></i>
+									</li>
+									<li>
+											<a href="tools/naflex/help/help.php">NAFlex</a>
+											<i class="fa fa-circle"></i>
+									</li>
+									<li>
+											<span>Tutorials</span>
+									</li>
+								</ul>
+						</div>
+						<!-- END PAGE BAR -->
+						<!-- BEGIN PAGE TITLE-->
+						<h1 class="page-title"> <span id="tit-static">NAFlex Tutorials</span>
+						</h1>
+						<!-- END PAGE TITLE-->
+						<!-- END PAGE HEADER-->
+
+						<div id="html-content-help">Videos Coming Soon</div>
+
+				</div>
+				<!-- END CONTENT BODY -->
+		</div>
+		<!-- END CONTENT -->
+
+
+
+<?php 
+
+require "../../../htmlib/footer.inc.php"; 
+require "../../../htmlib/js.inc.php";
+
+?>
---
+++ tools/pydockdna/help/help.php
@@ -0,0 +1,60 @@
+<?php
+
+require "../../../phplib/genlibraries.php";
+redirectOutside();
+
+?>
+
+<?php require "../../../htmlib/header.inc.php"; ?>
+
+<body class="page-header-fixed page-sidebar-closed-hide-logo page-content-white page-container-bg-solid page-sidebar-fixed">
+  <div class="page-wrapper">
+
+  <?php require "../../../htmlib/top.inc.php"; ?>
+  <?php require "../../../htmlib/menu.inc.php"; ?>
+
+<!-- BEGIN CONTENT -->
+                <div class="page-content-wrapper">
+                    <!-- BEGIN CONTENT BODY -->
+                    <div class="page-content">
+                        <!-- BEGIN PAGE HEADER-->
+                        <!-- BEGIN PAGE BAR -->
+                        <div class="page-bar">
+                            <ul class="page-breadcrumb">
+															<li>
+				  <a href="/home/">Home</a>
+				  <i class="fa fa-circle"></i>
+			      </li>
+                              <li>
+                                  <span>Help</span>
+                                  <i class="fa fa-circle"></i>
+                              </li>
+                              <li>
+                                  <a href="help/tools.php">Tools</a>
+																	<i class="fa fa-circle"></i>
+                              </li>
+															<li>
+                                  <span>pyDockDNA</span>
+                              </li>
+                            </ul>
+                        </div>
+                        <!-- END PAGE BAR -->
+                        <!-- BEGIN PAGE TITLE-->
+                        <h1 class="page-title"> pyDockDNA Help
+                        </h1>
+                        <!-- END PAGE TITLE-->
+                        <!-- END PAGE HEADER-->
+
+                            <p>pyDockDNA Help page.</p>
+
+                    </div>
+                    <!-- END CONTENT BODY -->
+                </div>
+                <!-- END CONTENT -->
+
+<?php 
+
+require "../../../htmlib/footer.inc.php"; 
+require "../../../htmlib/js.inc.php";
+
+?>
