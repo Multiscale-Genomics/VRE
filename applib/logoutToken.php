50961ce74c942024f107746b371c4da944cc6cc9
Laia <laia.codo@bsc.es>
2017-10-17 16:04:44 +0200

    fix redirectUri ServerName

--- applib/logoutToken.php
+++ applib/logoutToken.php
@@ -15,7 +15,9 @@ if($_REQUEST){
 //if($_POST){
 
     // End oauth2 session
-    $provider = new MuG_Oauth2Provider(['redirectUri'=> 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF']]);
+    //$provider = new MuG_Oauth2Provider(['redirectUri'=> 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF']]);
+    $provider = new MuG_Oauth2Provider(['redirectUri'=> $GLOBALS['URL'] . $_SERVER['PHP_SELF']]);
+
     /*
     $conf = getConf(__DIR__."/../../conf/oauth2.conf");
     $clientId     = $conf[0];
