4b97cf268117b2d80210ac5f89e8581ba01f7b26
root <root@multiscalegenomics.eu>
2018-02-27 18:39:35 +0100

    accepting registered users

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <html>
 <head>
   <meta charset="utf-8" />
-</head>
+  </head>
 <?php
-
 require "phplib/genlibraries.php";
-checkIfSessionUser(basename($_SERVER['PHP_SELF']));
 
-?>
+//checkIfSessionUser(basename($_SERVER['PHP_SELF']));
 
-<body style="background:#006b8f;">
-<form id="loginToken-form" action="applib/loginToken.php" method="post"></form>
-</body>
+// Check if PHP session exists.
+$r = checkLoggedIn();
 
-<script>
-document.getElementById("loginToken-form").submit();
-</script>
-</html>
+if (1){
+    if ($_REQUEST['id']){
+        // Recover guest user
+        $r = loadUser($_REQUEST['id'],false);
+    }else{
+        // Get access creating an a anonymous guest account
+        $r = createUserAnonymous();
+        if (!$r)
+            exit('Login error: cannot create anonymous VRE user');
+    }
+}
+//redirect("../home/");
+redirect("../home/redirect.php");
---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <meta charset="utf-8" />
+</head>
+<?php
+
+require "phplib/genlibraries.php";
+//checkIfSessionUser(basename($_SERVER['PHP_SELF']));
+
+?>
+
+<body style="background:#006b8f;">
+<form id="loginToken-form" action="applib/loginToken.php" method="post"></form>
+</body>
+
+<script>
+document.getElementById("loginToken-form").submit();
+</script>
+</html>
