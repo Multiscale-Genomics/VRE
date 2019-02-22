bcdda903dea8ca8f8140d5dd01c76171e9f40c1b
root <root@multiscalegenomics.eu>
2019-02-22 11:32:10 +0100

    administrate all jobs for admin users

---
+++ applib/delJob.php
@@ -0,0 +1,20 @@
+<?php
+
+require "../phplib/genlibraries.php";
+require "../phplib/admin.inc.php";
+
+redirectAdminOutside();
+
+
+if($_REQUEST["pid"]){
+
+	//delete job
+    $r = delJob($_REQUEST["pid"],NULL,"local",$_REQUEST["user"]);
+    if (!$r){
+        $_SESSION['errorData']['Error'][]= "Cannot cancel task. Unsuccessfully exit of 'deljob' for job $pid.";
+    }
+    redirect($GLOBALS['URL'].'/admin/adminJobs.php');
+}else{
+	redirect($GLOBALS['URL']);
+}
+?>
