a7f57e293488826ad8993df8c76005a55a2c7745
Laia <laia.codo@bsc.es>
2019-03-01 11:22:20 +0100

    fix bug logos from MuGVRE/tools-catalog/

--- publicsite/index.php
+++ publicsite/index.php
@@ -50,45 +50,42 @@ sort($toolList);
 
                             <div class="clearfix">
                                 <div id="js-filters-lightbox-gallery2" class="cbp-l-filters-button cbp-l-filters-left">
-																		<div data-filter="*" class="cbp-filter-item-active cbp-filter-item btn blue btn-outline uppercase">All</div>
-		
-																		<?php foreach($kw as $k) { ?>
-																		<div data-filter=".<?php echo $k; ?>" class="cbp-filter-item btn blue btn-outline uppercase"><?php echo /*str_replace("-", " ", $k);*/ $k; ?></div>
-																		<?php } ?>																	
-	
-                                </div>
-                            </div>
-														<div id="js-grid-lightbox-gallery" class="cbp">
-		
-																<?php 
+									<div data-filter="*" class="cbp-filter-item-active cbp-filter-item btn blue btn-outline uppercase">All</div>
 
-																foreach($toolList as $t) { 
+                                       <?php foreach($kw as $k) { ?>
+    										<div data-filter=".<?php echo $k; ?>" class="cbp-filter-item btn blue btn-outline uppercase"><?php echo /*str_replace("-", " ", $k);*/ $k; ?></div>
+										<?php } ?>
+                                    </div>
+                                    </div>
+									<div id="js-grid-lightbox-gallery" class="cbp">
 
-																$kw = implode(" ", $t['keywords']);
+    								  <?php 
+									  foreach($toolList as $t) { 
+                                            $kw = implode(" ", $t['keywords']);
 
-																if (strpos($kw, 'visualizer') === false) $type = 'tools';
-																else $type = 'visualizers';
+											if (strpos($kw, 'visualizer') === false) $type = 'tools';
+											else $type = 'visualizers';
 
-																?>
+											?>
 
-																	<div class="cbp-item <?php echo $kw; ?>">
-																	<!-- REMOVE cbp-singlePageInline to go to new page -->
-																		<a href="<?php echo $type; ?>/<?php echo $t['_id']; ?>/assets/home/index.html" class="cbp-caption cbp-singlePageInline" data-title="<?php echo $t['title']; ?>" rel="nofollow">
-                                        <div class="cbp-caption-defaultWrap">
-                                            <img src="<?php echo $type; ?>/<?php echo $t['_id']; ?>/assets/home/logo.png" alt="">
-                                        </div>
-                                        <div class="cbp-caption-activeWrap">
-                                            <div class="cbp-l-caption-alignLeft">
-                                                <div class="cbp-l-caption-body">
-																								<div class="cbp-l-caption-title"><?php echo $t['title']; ?></div>
-                                                    <div class="cbp-l-caption-desc"><?php echo $t['short_description']; ?></div>
+										<div class="cbp-item <?php echo $kw; ?>">
+    										<!-- REMOVE cbp-singlePageInline to go to new page -->
+											<a href="<?php echo $type; ?>/<?php echo $t['_id']; ?>/assets/home/index.html" class="cbp-caption cbp-singlePageInline" data-title="<?php echo $t['title']; ?>" rel="nofollow">
+                                                <div class="cbp-caption-defaultWrap">
+                                                    <img src="<?php echo $_REQUEST['type']; ?>/<?php echo $t['_id']; ?>/assets/home/logo.png" alt="">
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </a>
-																	</div>
+                                              <div class="cbp-caption-activeWrap">
+                                                <div class="cbp-l-caption-alignLeft">
+                                                    <div class="cbp-l-caption-body">
+													    <div class="cbp-l-caption-title"><?php echo $t['title']; ?></div>
+                                                        <div class="cbp-l-caption-desc"><?php echo $t['short_description']; ?></div>
+                                                    </div>
+                                                </div>
+                                              </div>
+                                           </a>
+										</div>
 
-																<?php } ?>
+									<?php } ?>
 
                             </div>
 
